cffe0c6f7f0fd68ecb6704980cb95fa16ebec646
Ilya Lisin <blacklacost@gmail.com>
2022-02-04 23:25:04 +0300

    signin & login

--- src/graphql.ts
+++ src/graphql.ts
@@ -1,4 +1,6 @@
+import { hash, compare } from 'bcrypt'
 import { gql, IResolvers } from 'apollo-server'
+import { sign } from 'jsonwebtoken'
 import { Context } from './context.interface'
 
 export const typeDefs = gql`
@@ -8,12 +10,18 @@ export const typeDefs = gql`
     name: String!
   }
 
+  type AuthPayload {
+    token: String!
+    user: User!
+  }
+
   type Query {
     users: [User!]
   }
 
   type Mutation {
-    userCreate(username: String!, password: String!, name: String!): User!
+    signup(username: String!, password: String!, name: String!): AuthPayload!
+    login(username: String!, password: String!): AuthPayload!
   }
 `
 export const resolvers: IResolvers<any, Context> = {
@@ -23,8 +31,32 @@ export const resolvers: IResolvers<any, Context> = {
     },
   },
   Mutation: {
-    userCreate: (_, args, { prisma }) => {
-      return prisma.user.create({ data: { ...args } })
+    signup: async (_, { username, password, name }, { prisma }) => {
+      const hashedPassword = await hash(password, 12)
+      const user = await prisma.user.create({ data: { username, password: hashedPassword, name } })
+      const token = sign({ userId: user.id }, 'super_secret')
+      return {
+        token,
+        user,
+      }
+    },
+
+    login: async (_, args, { prisma }) => {
+      const user = await prisma.user.findUnique({ where: { username: args.username } })
+      if (!user) {
+        throw new Error('Credential error')
+      }
+
+      const valid = await compare(args.password, user.password)
+      if (!valid) {
+        throw new Error('Credential error')
+      }
+
+      const token = sign({ userId: user.id }, 'super_secret')
+      return {
+        token,
+        user,
+      }
     },
   },
 }
